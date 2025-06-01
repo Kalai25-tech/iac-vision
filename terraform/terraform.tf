9b5907d6ed70bff6162c7edbffd20f5498d194da
kalai25-tech <kalaiyarasanjothimani@gmail.com>
2025-06-01 15:49:54 +0530

    Region fix

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -29,7 +29,7 @@ terraform {
   backend "s3" {
     bucket = "visiongit0123"
     key    = "terraform.tfstate"
-    region = "us-west-1"
+    region = "us-east-1"
   }
 
   #required_version = "~> 1.6.3"
